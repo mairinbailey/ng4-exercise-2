8d2135b8061e20d4796cb222fa71f146016ae510
Mairin Bailey <mairin.bailey@gmail.com>
2017-05-26 11:17:23 -0600

    initial commit and splitting app into components

---
+++ src/app/creation/creation.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-creation',
+  templateUrl: './creation.component.html',
+  styleUrls: ['./creation.component.css']
+})
+export class CreationComponent implements OnInit {
+  newServerName = '';
+  newServerContent = '';
+  
+  constructor() { }
+
+  ngOnInit() {
+  }
+  onAddServer() {
+    this.serverElements.push({
+      type: 'server',
+      name: this.newServerName,
+      content: this.newServerContent
+    });
+  }
+
+  onAddBlueprint() {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: this.newServerName,
+      content: this.newServerContent
+    });
+  }
+}
