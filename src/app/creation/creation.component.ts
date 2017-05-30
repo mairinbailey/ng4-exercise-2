82c9ff1e463408d6801e55b7ed1c531c446ed503
Mairin Bailey <mairin.bailey@gmail.com>
2017-05-30 14:48:05 -0600

    add local reference fetched through view child

--- src/app/creation/creation.component.ts
+++ src/app/creation/creation.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-creation',
@@ -11,8 +11,8 @@ export class CreationComponent implements OnInit {
   @Output() blueprintCreated= new EventEmitter <{serverName: string, serverContent: string}>();
 
   // newServerName = '';
-  newServerContent = '';
-
+  // newServerContent = '';
+  @ViewChild('serverContentInput') serverContentInput: ElementRef;
   constructor() { }
 
   ngOnInit() {
@@ -20,14 +20,14 @@ export class CreationComponent implements OnInit {
   onAddServer(nameInput: HTMLInputElement) {
     this.serverCreated.emit({
       serverName: nameInput.value,
-      serverContent: this.newServerContent
+      serverContent: this.serverContentInput.nativeElement.value
     });
   }
 
   onAddBlueprint(nameInput: HTMLInputElement) {
     this.blueprintCreated.emit({
       serverName: nameInput.value,
-      serverContent: this.newServerContent
+      serverContent: this.serverContentInput.nativeElement.value
     });
   }
 }
