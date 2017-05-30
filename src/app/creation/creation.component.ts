2d29ecb162735aa8a55f506dc99b6054044efb25
Mairin Bailey <mairin.bailey@gmail.com>
2017-05-30 10:15:57 -0600

    add custom data binding from child to parent component

--- src/app/creation/creation.component.ts
+++ src/app/creation/creation.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-creation',
@@ -6,6 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./creation.component.css']
 })
 export class CreationComponent implements OnInit {
+  @Output() serverCreated= new EventEmitter <{serverName: string, serverContent: string}>();
+
+  @Output() blueprintCreated= new EventEmitter <{serverName: string, serverContent: string}>();
+
   newServerName = '';
   newServerContent = '';
 
@@ -14,18 +18,16 @@ export class CreationComponent implements OnInit {
   ngOnInit() {
   }
   onAddServer() {
-    // this.serverElements.push({
-    //   type: 'server',
-    //   name: this.newServerName,
-    //   content: this.newServerContent
-    // });
+    this.serverCreated.emit({
+      serverName: this.newServerName,
+      serverContent: this.newServerContent
+    });
   }
 
   onAddBlueprint() {
-  //   this.serverElements.push({
-  //     type: 'blueprint',
-  //     name: this.newServerName,
-  //     content: this.newServerContent
-  //   });
+    this.blueprintCreated.emit({
+      serverName: this.newServerName,
+      serverContent: this.newServerContent
+    });
   }
 }
